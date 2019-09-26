f864497a36eaebf70db4a82ec4c38f838c4fb885
ScaryGosling <38102210+ScaryGosling@users.noreply.github.com>
2019-09-26 12:51:24 +0200

    ree

---
+++ SPL_Lärspel/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    private static int index = 0;
+
+    [SerializeField] private string[] hints;
+    [SerializeField] private Image[][] images;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void reDraw()
+    {
+        changeText();
+        changeImg();
+    }
+
+    void changeText()
+    {
+
+    }
+
+    void changeImg()
+    {
+
+    }
+
+
+}
