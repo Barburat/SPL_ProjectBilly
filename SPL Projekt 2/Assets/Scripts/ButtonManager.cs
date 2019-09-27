892383038ca5564c7cb03d42bfc1adb17cd3785a
Jocke <jockeljung1@gmail.com>
2019-09-27 20:08:00 +0200

    main stuff

---
+++ SPL Projekt 2/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonManager : MonoBehaviour
+{
+    public void FirstInvention()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+    public void SecondInvention()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+    public void ThirdInvention()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+    public void FourthInvention()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+}
