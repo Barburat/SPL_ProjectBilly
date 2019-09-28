cbfab44c35fb823f30037ae72d5fd4bee0d6ef86
Trollsnor <sofiachylekauko@gmail.com>
2019-09-28 17:49:17 +0200

    book

---
+++ SPL Projekt 2/Assets/Scripts/BookScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookScript : MonoBehaviour
+{
+
+    public string[] Entries = new string[5];
+    public Texture[] pics = new Texture[5];
+    private int bookIndex = 0;
+    public Text textfield;
+    public RawImage imagefield;
+
+    // save image and at waht bookpage (bookindex) the should show
+    //public Dictionary<int, Texture> images = new Dictionary<int, Texture>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextEntry()
+    {
+        if (bookIndex == 4)
+        {
+            bookIndex = 0;
+        }
+        bookIndex++;
+        textfield.text = Entries[bookIndex];
+        showImage(bookIndex);
+
+    }
+
+    public void PreviousEntry()
+    {
+        if(bookIndex == 0)
+        {
+            bookIndex = 5;
+        }
+        bookIndex--;
+        textfield.text = Entries[bookIndex];
+        showImage(bookIndex);
+    }
+
+    private void showImage(int index)
+    {
+
+        imagefield.texture = pics[index];
+
+        /*
+        if (images.ContainsKey(index))
+        {
+            imagefield.texture = images[index];
+        }
+        */
+    }
+
+
+}
